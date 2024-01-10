396853015a45acccf3bc7ecaaac0220f3c5ae6f9
Inspirol <91295122+Inspirol@users.noreply.github.com>
2024-01-09 22:18:47 -0500

    moving things around

--- robotpy_toolkit_7407_v2/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = '0.5.11'
-
-from robotpy_toolkit_7407_v2.subsystem import Subsystem
-from robotpy_toolkit_7407_v2.command import BasicCommand, SubsystemCommand
-from robotpy_toolkit_7407_v2.motor import Motor, PIDMotor, EncoderMotor
-import robotpy_toolkit_7407_v2.unum
